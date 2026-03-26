7a5a3fe5d3bf864d787ef5d54155b8605f33e826
Shafeeq Azzam <shafeeqazzam@microsoft.com>
2026-03-26 16:46:25 +0200

    a

--- Notebook_2.Notebook/notebook-content.py
+++ Notebook_2.Notebook/notebook-content.py
@@ -1,1 +1,23 @@
-# No content - het
+# Fabric notebook source
+
+# METADATA ********************
+
+# META {
+# META   "kernel_info": {
+# META     "name": "synapse_pyspark"
+# META   },
+# META   "dependencies": {}
+# META }
+
+# CELL ********************
+
+# Welcome to your new notebook
+# Type here in the cell editor to add code!
+
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
